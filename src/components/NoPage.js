8c55113716abc72cbcff0dba5f51344faff3d895
Basilogast <duyhung08112003@gmail.com>
2025-04-25 07:55:49 +0700

    Add No

--- src/components/NoPage.js
+++ src/components/NoPage.js
@@ -5,6 +5,7 @@ export const NoPage = () => {
     <div style={{ textAlign: 'center', marginTop: '0px' }}>
       <h1>😢 You clicked "Hum"! 😢</h1>
       <p>Maybe next time? We're here for you! ❤️</p>
+      <p>Hong design page này tại bà sẽ không bao giờ nhấn No đúng hem, bà còn chả thấy được dòng :33</p>
     </div>
   );
 };
