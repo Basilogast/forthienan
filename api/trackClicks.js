39d26200cbc6a464da0b5141256919dc1828d30d
Basilogast <duyhung08112003@gmail.com>
2025-04-26 04:25:10 +0700

    Update env variablevariable

--- api/trackClicks.js
+++ api/trackClicks.js
@@ -7,7 +7,11 @@ import { getApps, initializeApp } from "firebase-admin/app";
 // Prevent reinitializing admin SDK
 if (!getApps().length) {
   initializeApp({
-    credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)),
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    }),
   });
 }
 
