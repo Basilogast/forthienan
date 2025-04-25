132ec07ab0e75a5b37db3811898c9f78034d9142
Basilogast <duyhung08112003@gmail.com>
2025-04-26 05:25:25 +0700

    update front-end

--- src/components/LoveLetter.js
+++ src/components/LoveLetter.js
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import "./LoveLetter.css";
+import loveSong from "../assets/audio/nguhoitanhy.MP3";
 
 export const LoveLetter = () => {
   const [isFlapped, setIsFlapped] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false); // State to track if audio is playing
   const navigate = useNavigate();
+  const audioRef = useRef(null);
 
   const toggleFlap = () => {
     setIsFlapped(!isFlapped);
@@ -30,9 +33,28 @@ export const LoveLetter = () => {
     }
   };
 
+  const toggleAudio = () => {
+    if (audioRef.current) {
+      if (isPlaying) {
+        audioRef.current.pause(); // Pause the audio
+        setIsPlaying(false); // Update state
+      } else {
+        audioRef.current.play(); // Play the audio
+        setIsPlaying(true); // Update state
+      }
+    }
+  };
+
   return (
     <div>
       <div className="container">
+        {/* Add the audio player */}
+        <div className="audio-player">
+          <audio ref={audioRef} src={loveSong} preload="auto"></audio>
+          <button className="btn" onClick={toggleAudio}>
+            {isPlaying ? "Ồn quá hem" : "Nhạc nè"} {/* Toggle button text */}
+          </button>
+        </div>
         <div
           className={`envelope-wrapper ${isFlapped ? "flap" : ""}`}
           onClick={toggleFlap}
@@ -42,8 +64,8 @@ export const LoveLetter = () => {
               <div className="text">
                 <strong>Gửi pạn Thiên Ân,</strong>
                 <p>
-                  Lâu rùi mới làm web kiểu này :"3, hem biết t đoán có đúng hong, nhưng hì, cậu có thể
-                  đồng ý làm tương lai của tớ hem?
+                  Để phần ngại ngùng tớ lo cho, cậu chỉ cần cho tớ trở thành 1 phần trong
+                  tương lai của cậu là được rùi:3, nhaa?
                 </p>
               </div>
             </div>
